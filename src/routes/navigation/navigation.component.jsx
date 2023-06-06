93fa0b64de45e05b51c60b2a17f39ad9b0225107
usman-ahmed99 <usman.ahmed99@hotmail.com>
2023-06-06 22:25:27 +0500

    added cart dropdown functionality

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,11 +3,15 @@ import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { useContext } from "react";
 import { UserContext } from "../../context/user.context";
+import { CartContext } from "../../context/cart.context";
 import "./navigation.styles.scss";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext);
   const signOutHandler = async () => {
     await signOutUser();
   };
@@ -30,7 +34,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
